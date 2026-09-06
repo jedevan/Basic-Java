c9056d05b4f1ef8173dc64f38a7de3238e2476d2
jedevan <jedevancagulada@gmail.com>
2026-09-06 22:49:44 +0800

    Add Main class with Person object instantiation

---
+++ Basic Java/basic oop java1/Main.java
@@ -0,0 +1,14 @@
+package oop cagulada;
+
+public class Main{
+
+	public static void main (String[] args){
+		
+		Person p = new Person()
+		p.setName("Maria Santos");
+		p.setAge(18);
+		
+		System.out.println(p.describe());
+}
+
+}
